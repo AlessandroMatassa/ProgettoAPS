b09602374359db8f867777b71fba1e07556e3ee3
AlessandroMatassa <164194957+AlessandroMatassa@users.noreply.github.com>
2024-06-05 13:33:00 +0200

    Create OperatoreController.java

---
+++ Lab 4/OperatoreController.java
@@ -0,0 +1,13 @@
+public class OperatoreController {
+
+    public void richiestaModifica(String codicePrenotazione, RichiestaDiModifica richiestaDiModifica) {
+        // Implement method logic here
+    }
+
+    public void valutaRichiesta() {
+        // Implement method logic here
+    }
+
+    public void prendiInCaricoRichiesta() {
+        // Implement method logic here
+    }
